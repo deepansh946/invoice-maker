c39f2d977c1365e122cbefd4b5a9d1a58621c2e4
deepansh <deepansh@kontext.in>
2019-11-11 01:18:50 +0530

    Build folder added to git ignor

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const generateInvoice = async (req, res) => {
 
 app.post('/get-invoice', generateInvoice);
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
